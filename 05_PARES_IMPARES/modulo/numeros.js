43202f5f4d004e963820dacc7ba67c36c3967777
ESTELA ALVES DE MORAES <23111781@CFP127ALUNO.LOCAL>
2023-09-13 12:27:10 -0300

    a

---
+++ 05_PARES_IMPARES/modulo/numeros.js
@@ -0,0 +1,20 @@
+/************************************************************
+ * Objetivo: Criar um sistema que gerencie números pares e ímpares
+ * Data: 13/09/2023
+ * Autor: Estela Alves de Moraes
+ * Versão: 1.0
+ ***********************************************************/
+
+const identificarNumeros = function(numeroInicial, numeroFinal){
+
+    inicial = numeroInicial
+    final = numeroFinal
+
+    if (inicial == '' || final == '')
+
+    else if(isNaN(inicial) || isNaN(final))
+    
+    else if(inicial > 500 || inicial < 0)
+
+    else if (final > 1000 || final < 10)
+}
